099ed97333ff5959d0b46d9c70d4be3a89012dd4
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-03-25 16:27:32 -0300

    Entidade paciente, repositório e controller devidamente criados

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,38 @@
+package med.voll.api.paciente;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    @Embedded
+    private Endereco endereco;
+
+
+    public Paciente(DadosCadastroPaciente dadosPaciente) {
+        this.nome = dadosPaciente.nome();
+        this.email = dadosPaciente.email();
+        this.cpf = dadosPaciente.cpf();
+        this.telefone = dadosPaciente.telefone();
+        this.endereco = new Endereco(dadosPaciente.endereco());
+
+    }
+}
