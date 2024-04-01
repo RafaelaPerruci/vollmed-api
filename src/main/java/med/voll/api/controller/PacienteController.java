11c8f97db2082de4e10dd8ba7dd83ddc82c6a6c4
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-04-01 20:36:51 -0300

    implementação de paciente e DTO para listagem

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,14 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,5 +24,9 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosPaciente){
         pacienteRepository.save(new Paciente(dadosPaciente));
 
+    }
+    @GetMapping
+    public List<DadosListagemPaciente> listar(){
+        return pacienteRepository.findAll().stream().map(DadosListagemPaciente::new).toList();
     }
 }
